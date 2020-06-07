80a9cfe1d474b4375b419551cec1156206f71207
jiyucho9145 <dash17pr131@gmail.com>
2020-06-07 11:35:02 +0900

    Added the entities module.

--- studyscheduler/__init__.py
+++ studyscheduler/__init__.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
 
 from studyscheduler.logics import Facade
+from studyscheduler.entities import Subject
+from studyscheduler.entities import Textbook
+from studyscheduler.entities import Section
---
+++ studyscheduler/entities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import codecs
+import json
+import sqlite3
+
+class Subject:
+
+    def __init__(self):
+        pass
+
+class Textbook:
+
+    def __init__(self):
+        pass
+
+class Section:
+
+    def __init__(self):
+        pass
