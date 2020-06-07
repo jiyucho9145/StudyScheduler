bd956f94654d330411f8902951ff95a1dc57aca6
jiyucho9145 <dash17pr131@gmail.com>
2020-06-07 09:45:07 +0900

    Added the logic module.

--- app.py
+++ app.py
@@ -3,10 +3,15 @@
 import sys
 import os
 import codecs
+import json
+import sqlite3
+from studyscheduler import Facade
 
 class App:
 
     def run(self):
+        facade = Facade()
+        facade.run()
         return 0
 
 if __name__ == '__main__':
---
+++ studyscheduler/logics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import codecs
+import json
+import sqlite3
+
+class Facade:
+
+    def run(self):
+        print('hello')
