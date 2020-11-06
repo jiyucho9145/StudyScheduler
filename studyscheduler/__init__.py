12dc946db1bfe151d2ebcef11cd6ff7f14c4f1ae
jiyucho9145 <yamatopanda536@gmail.com>
2020-11-07 07:34:38 +0900

    Changed programming languages.

--- studyscheduler/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from studyscheduler.logics import Facade
-from studyscheduler.entities import Subject
-from studyscheduler.entities import Textbook
-from studyscheduler.entities import Section
