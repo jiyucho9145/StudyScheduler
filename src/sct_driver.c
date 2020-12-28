34d35303352cb29343204b96d4ff5fba9e351dcd
jiyucho9145 <yamatopanda536@gmail.com>
2020-12-28 20:13:43 +0900

    Updated a driver program.

--- src/sct_driver.c
+++ src/sct_driver.c
@@ -2,21 +2,35 @@
 #include <sct_database_profile.h>
 #include <sct_accounting_profile.h>
 
+static int run(sct_manager *mgr, int argc, char[] *argv)
+{
+
+    sct_database_profile *dp = sct_new_database_profile(mgr);
+    SCT_CHK_NULL(dp)
+
+    return 0;
+
+}
+
 int main(int argc, char[] *argv)
 {
 
     sct_int rc;
+    sct_int rc2;
 
     sct_manager *mgr = sct_alloc_manager();
     if (NULL == mgr){
         return 1;
     }
 
-    sct_database_profile *dp = sct_new_database_profile(mgr);
-    SCT_CHK_NULL(dp)
+    rc2 = run(mgr, argc, argv);
 
     rc = sct_free_manager(mgr);
     if (0 != rc){
+        return 1;
+    }
+
+    if (0 != rc2){
         return 1;
     }
 
